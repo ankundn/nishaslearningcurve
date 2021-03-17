cdf0652f6bc5a897223f3b0ffd5ea109ab12c96a
ankundn <ankundnisha0@gmail.com>
2021-03-17 17:05:41 +0300

    Added logo to navbar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,65 @@
-import React, { Component } from "react";
-import "./App.css";
-import SignUp from "./components/Home/SignUp/SignUp";
-import SignIn from "./components/Home/SignIn/SignIn";
-import Home from "./components/Home/Home";
-import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
+// import React, { Component } from "react";
+// import "./App.css";
+// import SignUp from "./components/Home/SignUp/SignUp";
+// import SignIn from "./components/Home/SignIn/SignIn";
+// import Home from "./components/Home/Home";
+// import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
-export default class App extends Component {
-  render() {
-    let navLink = (
-      <div className="Tab">
-        <NavLink to="/sign-in" activeClassName="activeLink" className="signIn">
-          Sign In
-        </NavLink>
-        <NavLink exact to="/" activeClassName="activeLink" className="signUp">
-          Sign Up
-        </NavLink>
-      </div>
-    );
-    const login = localStorage.getItem("isLoggedIn");
+// export default class App extends Component {
+//   render() {
+//     let navLink = (
+//       <div className="Tab">
+//         <NavLink to="/sign-in" activeClassName="activeLink" className="signIn">
+//           Sign In
+//         </NavLink>
+//         <NavLink exact to="/" activeClassName="activeLink" className="signUp">
+//           Sign Up
+//         </NavLink>
+//       </div>
+//     );
+//     const login = localStorage.getItem("isLoggedIn");
 
-    return (
-      <div className="App">
-        {login ? (
-          <Router>
-            <Route exact path="/" component={SignUp}></Route>
-            <Route path="/sign-in" component={SignIn}></Route>
-            <Route path="/home" component={Home}></Route>
-          </Router>
-        ) : (
-          <Router>
-            {navLink}
-            <Route exact path="/" component={SignUp}></Route>
-            <Route path="/sign-in" component={SignIn}></Route>
-            <Route path="/home" component={Home}></Route>
-          </Router>
-        )}
-      </div>
-    );
-  }
+//     return (
+//       <div className="App">
+//         {login ? (
+//           <Router>
+//             <Route exact path="/" component={SignUp}></Route>
+//             <Route path="/sign-in" component={SignIn}></Route>
+//             <Route path="/home" component={Home}></Route>
+//           </Router>
+//         ) : (
+//           <Router>
+//             {navLink}
+//             <Route exact path="/" component={SignUp}></Route>
+//             <Route path="/sign-in" component={SignIn}></Route>
+//             <Route path="/home" component={Home}></Route>
+//           </Router>
+//         )}
+//       </div>
+//     );
+//   }
+// }
+
+
+
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+
+
+function App() {
+  return (
+    <Router>
+ <Navbar />  
+ <Switch>
+   <Route path='/' /> 
+ </Switch>
+    </Router>
+     
+   
+  )
 }
+
+export default App; 

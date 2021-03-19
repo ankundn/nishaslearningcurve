c2bd3915b03a5c7353a25ba436f5c93fe84515cc
ankundn <ankundnisha0@gmail.com>
2021-03-19 10:08:26 +0300

    Added footer component

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/HomePage/Home';
-
+import Footer from './components/pages/Footer/Footer';
 
 
 function App() {
@@ -57,6 +57,8 @@ function App() {
  <Switch>
    <Route path='/'exact component={Home}/> 
  </Switch>
+ <Footer />
+
     </Router>
      
    
